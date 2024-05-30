244e433b1167558582ba40497a58587dc20f2388
Sacid66 <148653854+Sacid66@users.noreply.github.com>
2024-05-30 14:06:39 +0300

    not

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import os
-import eventlet
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='eventlet')
@@ -11,18 +8,9 @@ socketio = SocketIO(app, async_mode='eventlet')
 def index():
     return render_template('index.html')
 
-@socketio.on('start')
-def handle_start(data):
-    emit('start', data, broadcast=True)
-
-@socketio.on('draw')
-def handle_draw(data):
-    emit('draw', data, broadcast=True)
-
-@socketio.on('end')
-def handle_end(data):
-    emit('end', data, broadcast=True)
+@socketio.on('draw_event')
+def handle_draw_event(json):
+    emit('broadcast_draw', json, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, debug=True)
